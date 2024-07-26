58577b6aa4568c19e98d55905500c529ad2471f8
Thith-thith <thiththin762@gmail.com>
2024-07-26 00:07:28 -0400

    update

--- graphql.bk/mutation/location.ts
+++ graphql.bk/mutation/location.ts
@@ -1,13 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_CUSTOMER_LOCATION = gql`
-  mutation (
-    $bodyLocation: BodyLocation!
-    $bodyAddress: BodyAddress!
-  ) {
-    createLocation(
-      bodyLocation: $bodyLocation
-      bodyAddress: $bodyAddress
-    )
+  mutation ($bodyLocation: BodyLocation!, $bodyAddress: BodyAddress!) {
+    createLocation(bodyLocation: $bodyLocation, bodyAddress: $bodyAddress)
   }
 `;
