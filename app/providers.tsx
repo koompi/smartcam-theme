a1f894e5dbfcfb8ce560359475a94bbdf987b8e4
Thith-thith <thiththin762@gmail.com>
2024-08-15 00:18:05 -0400

    updated script

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,19 +4,29 @@ import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 import { TelegramProvider } from "@/context/telegramProvider";
+import Script from "next/script";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
   return (
-    <NextUIProvider navigate={router.push}>
-      <ProgressBar
-        height="3px"
-        color="#0F3ECC"
-        options={{ showSpinner: false }}
-        shallowRouting
+    <>
+      <Script
+        src="https://telegram.org/js/telegram-web-app.js"
+        strategy="beforeInteractive"
+        onLoad={() => console.log("loaded")}
+        onError={() => console.log("error")}
+        onReady={() => console.log("ready")}
       />
-      <TelegramProvider>{children}</TelegramProvider>
-    </NextUIProvider>
+      <NextUIProvider navigate={router.push}>
+        <ProgressBar
+          height="3px"
+          color="#0F3ECC"
+          options={{ showSpinner: false }}
+          shallowRouting
+        />
+        <TelegramProvider>{children}</TelegramProvider>
+      </NextUIProvider>
+    </>
   );
 }
