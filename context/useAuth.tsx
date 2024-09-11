21fb5734d35b739d7f62892a03a695dd9c1c1162
vannsoklay <soklayvann@gmail.com>
2024-09-11 10:13:30 +0700

    update

--- context/useAuth.tsx
+++ context/useAuth.tsx
@@ -59,13 +59,17 @@ export const AppProvider: FC<Props> = (props) => {
           method: "POST",
           headers: myHeaders,
           body: JSON.stringify(body),
-          redirect: "follow"
+          redirect: "follow",
         } as RequestInit;
 
-        fetch("https://backendv2.riverbase.org/sso/telegram/login", requestOptions)
+        fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND}/sso/telegram/login`,
+          requestOptions
+        )
           .then((response) => response.text())
           .then((result) => {
-            alert("success")
+            setProcessing("success");
+            console.error(result);
           })
           .catch((error) => console.error(error));
 
@@ -96,7 +100,7 @@ export const AppProvider: FC<Props> = (props) => {
     if (typeof window !== "undefined" && window.Telegram?.WebApp) {
       handleTelegramLogin();
     }
-  }, [])
+  }, []);
 
   const logout = () => {
     axios.post(`${process.env.NEXT_PUBLIC_BACKEND}/sso/logout`).then((_) => {
@@ -111,9 +115,10 @@ export const AppProvider: FC<Props> = (props) => {
       value={{
         user: user,
         loading: loading,
-        logout: logout
+        logout: logout,
       }}
     >
+      {JSON.stringify(processing)}
       {JSON.stringify(user)}
       {props.children}
     </AuthContext.Provider>
