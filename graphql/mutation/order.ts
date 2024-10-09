bf19d52b169b43b07ea20797a21419571e87941e
Thith-thith <thiththin762@gmail.com>
2024-10-09 05:30:36 -0400

    updated

--- graphql/mutation/order.ts
+++ graphql/mutation/order.ts
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const CONFIRM_ORDER = gql`
-  mutation ($orderId: String!) {
-    customerConfirmOrder(orderId: $orderId)
+  mutation StoreConfirmOrder($orderId: String!) {
+    storeConfirmOrder(orderId: $orderId)
   }
 `;
 
